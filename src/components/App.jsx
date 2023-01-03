3579f215f75b591db6e07175fae86788a6c16f1c
sofidorosh <108129293+anenkosofi@users.noreply.github.com>
2023-01-03 09:44:30 +0200

    Delete prop-types of App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { GlobalStyle } from './GlobalStyle';
 import { Box } from './Box.styled';
 import { ContactForm } from './ContactForm';
@@ -64,18 +63,3 @@ export class App extends React.Component {
     );
   }
 }
-
-App.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-  filter: PropTypes.string,
-  addContact: PropTypes.func,
-  findContact: PropTypes.func,
-  filterContacts: PropTypes.func,
-  deleteContact: PropTypes.func,
-};
